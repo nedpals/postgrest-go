510dbdc194b03ae6dd9298560a59201c6005b0da
qniwerss <73220736+werniq@users.noreply.github.com>
2023-06-14 15:43:08 +0200

    Update request_builder.go
    Just added comments

--- pkg/request_builder.go
+++ pkg/request_builder.go
@@ -11,7 +11,7 @@ import (
 	"strings"
 )
 
-// TODO test
+// RequestError represents an error response from the PostgREST server.
 type RequestError struct {
 	Message        string `json:"message"`
 	Details        string `json:"details"`
@@ -24,6 +24,7 @@ func (rq *RequestError) Error() string {
 	return fmt.Sprintf("%s: %s", rq.Code, rq.Message)
 }
 
+// RequestBuilder represents a builder for PostgREST requests.
 type RequestBuilder struct {
 	client *Client
 	path   string
@@ -31,6 +32,7 @@ type RequestBuilder struct {
 	header http.Header
 }
 
+// Select starts building a SELECT request with the specified columns.
 func (b *RequestBuilder) Select(columns ...string) *SelectRequestBuilder {
 	b.params.Set("select", strings.Join(columns, ","))
 	return &SelectRequestBuilder{
@@ -47,6 +49,7 @@ func (b *RequestBuilder) Select(columns ...string) *SelectRequestBuilder {
 	}
 }
 
+// Insert starts building an INSERT request with the provided JSON data.
 func (b *RequestBuilder) Insert(json interface{}) *QueryRequestBuilder {
 	b.header.Set("Prefer", "return=representation")
 	return &QueryRequestBuilder{
@@ -59,6 +62,7 @@ func (b *RequestBuilder) Insert(json interface{}) *QueryRequestBuilder {
 	}
 }
 
+// Upsert starts building an UPSERT request with the provided JSON data.
 func (b *RequestBuilder) Upsert(json interface{}) *QueryRequestBuilder {
 	b.header.Set("Prefer", "return=representation,resolution=merge-duplicates")
 	return &QueryRequestBuilder{
@@ -71,6 +75,7 @@ func (b *RequestBuilder) Upsert(json interface{}) *QueryRequestBuilder {
 	}
 }
 
+// Update starts building an UPDATE request with the provided JSON data.
 func (b *RequestBuilder) Update(json interface{}) *FilterRequestBuilder {
 	b.header.Set("Prefer", "return=representation")
 	return &FilterRequestBuilder{
@@ -86,6 +91,7 @@ func (b *RequestBuilder) Update(json interface{}) *FilterRequestBuilder {
 	}
 }
 
+// Delete starts building a DELETE request.
 func (b *RequestBuilder) Delete() *FilterRequestBuilder {
 	return &FilterRequestBuilder{
 		QueryRequestBuilder: QueryRequestBuilder{
@@ -100,6 +106,7 @@ func (b *RequestBuilder) Delete() *FilterRequestBuilder {
 	}
 }
 
+// QueryRequestBuilder represents a builder for query requests.
 type QueryRequestBuilder struct {
 	client     *Client
 	params     url.Values
@@ -109,10 +116,12 @@ type QueryRequestBuilder struct {
 	json       interface{}
 }
 
+// Execute sends the query request and unmarshals the response JSON into the provided object.
 func (b *QueryRequestBuilder) Execute(r interface{}) error {
 	return b.ExecuteWithContext(context.Background(), r)
 }
 
+// ExecuteWithContext sends the query request with the provided context and unmarshals the response JSON into the provided object.
 func (b *QueryRequestBuilder) ExecuteWithContext(ctx context.Context, r interface{}) error {
 	data, err := json.Marshal(b.json)
 	if err != nil {
@@ -126,7 +135,7 @@ func (b *QueryRequestBuilder) ExecuteWithContext(ctx context.Context, r interfac
 	req.URL.RawQuery = b.params.Encode()
 	req.Header = b.client.Headers()
 
-	// inject/override custom headers
+	// Inject/override custom headers
 	for key, vals := range b.header {
 		for _, val := range vals {
 			req.Header.Set(key, val)
@@ -167,16 +176,19 @@ func (b *QueryRequestBuilder) ExecuteWithContext(ctx context.Context, r interfac
 	return nil
 }
 
+// FilterRequestBuilder represents a builder for filter requests.
 type FilterRequestBuilder struct {
 	QueryRequestBuilder
 	negateNext bool
 }
 
+// Not negates the next filter condition.
 func (b *FilterRequestBuilder) Not() *FilterRequestBuilder {
 	b.negateNext = true
 	return b
 }
 
+// Filter adds a filter condition to the request.
 func (b *FilterRequestBuilder) Filter(column, operator, criteria string) *FilterRequestBuilder {
 	if b.negateNext {
 		b.negateNext = false
@@ -186,58 +198,72 @@ func (b *FilterRequestBuilder) Filter(column, operator, criteria string) *Filter
 	return b
 }
 
+// Eq adds an equality filter condition to the request.
 func (b *FilterRequestBuilder) Eq(column, value string) *FilterRequestBuilder {
 	return b.Filter(column, "eq", SanitizeParam(value))
 }
 
+// Neq adds a not-equal filter condition to the request.
 func (b *FilterRequestBuilder) Neq(column, value string) *FilterRequestBuilder {
 	return b.Filter(column, "neq", SanitizeParam(value))
 }
 
+// Gt adds a greater-than filter condition to the request.
 func (b *FilterRequestBuilder) Gt(column, value string) *FilterRequestBuilder {
 	return b.Filter(column, "gt", SanitizeParam(value))
 }
 
+// Gte adds a greater-than-or-equal filter condition to the request.
 func (b *FilterRequestBuilder) Gte(column, value string) *FilterRequestBuilder {
 	return b.Filter(column, "gte", SanitizeParam(value))
 }
 
+// Lt adds a less-than filter condition to the request.
 func (b *FilterRequestBuilder) Lt(column, value string) *FilterRequestBuilder {
 	return b.Filter(column, "lt", SanitizeParam(value))
 }
 
+// Lte adds a less-than-or-equal filter condition to the request.
 func (b *FilterRequestBuilder) Lte(column, value string) *FilterRequestBuilder {
 	return b.Filter(column, "lte", SanitizeParam(value))
 }
 
+// Is adds an IS filter condition to the request.
 func (b *FilterRequestBuilder) Is(column, value string) *FilterRequestBuilder {
 	return b.Filter(column, "is", SanitizeParam(value))
 }
 
+// Like adds a LIKE filter condition to the request.
 func (b *FilterRequestBuilder) Like(column, value string) *FilterRequestBuilder {
 	return b.Filter(column, "like", SanitizeParam(value))
 }
 
+// Ilike adds a ILIKE filter condition to the request.
 func (b *FilterRequestBuilder) Ilike(column, value string) *FilterRequestBuilder {
 	return b.Filter(column, "ilike", SanitizeParam(value))
 }
 
+// Fts adds a full-text search filter condition to the request.
 func (b *FilterRequestBuilder) Fts(column, value string) *FilterRequestBuilder {
 	return b.Filter(column, "fts", SanitizeParam(value))
 }
 
+// Plfts adds a phrase-level full-text search filter condition to the request.
 func (b *FilterRequestBuilder) Plfts(column, value string) *FilterRequestBuilder {
 	return b.Filter(column, "plfts", SanitizeParam(value))
 }
 
+// Phfts adds a phrase-headline-level full-text search filter condition to the request.
 func (b *FilterRequestBuilder) Phfts(column, value string) *FilterRequestBuilder {
 	return b.Filter(column, "phfts", SanitizeParam(value))
 }
 
+// Wfts adds a word-level full-text search filter condition to the request.
 func (b *FilterRequestBuilder) Wfts(column, value string) *FilterRequestBuilder {
 	return b.Filter(column, "wfts", SanitizeParam(value))
 }
 
+// In adds an IN filter condition to the request.
 func (b *FilterRequestBuilder) In(column string, values []string) *FilterRequestBuilder {
 	sanitized := make([]string, len(values))
 	for i, value := range values {
@@ -246,6 +272,7 @@ func (b *FilterRequestBuilder) In(column string, values []string) *FilterRequest
 	return b.Filter(column, "in", fmt.Sprintf("(%s)", strings.Join(sanitized, ",")))
 }
 
+// Cs adds a contains set filter condition to the request.
 func (b *FilterRequestBuilder) Cs(column string, values []string) *FilterRequestBuilder {
 	sanitized := make([]string, len(values))
 	for i, value := range values {
@@ -254,6 +281,7 @@ func (b *FilterRequestBuilder) Cs(column string, values []string) *FilterRequest
 	return b.Filter(column, "cs", fmt.Sprintf("{%s}", strings.Join(sanitized, ",")))
 }
 
+// Cd adds a contained by set filter condition to the request.
 func (b *FilterRequestBuilder) Cd(column string, values []string) *FilterRequestBuilder {
 	sanitized := make([]string, len(values))
 	for i, value := range values {
@@ -262,6 +290,7 @@ func (b *FilterRequestBuilder) Cd(column string, values []string) *FilterRequest
 	return b.Filter(column, "cd", fmt.Sprintf("{%s}", strings.Join(sanitized, ",")))
 }
 
+// Ov adds an overlaps set filter condition to the request.
 func (b *FilterRequestBuilder) Ov(column string, values []string) *FilterRequestBuilder {
 	sanitized := make([]string, len(values))
 	for i, value := range values {
@@ -270,41 +299,72 @@ func (b *FilterRequestBuilder) Ov(column string, values []string) *FilterRequest
 	return b.Filter(column, "ov", fmt.Sprintf("{%s}", strings.Join(sanitized, ",")))
 }
 
+// Sl adds a strictly left of filter condition to the request.
 func (b *FilterRequestBuilder) Sl(column string, from, to int) *FilterRequestBuilder {
 	return b.Filter(column, "sl", fmt.Sprintf("(%d,%d)", from, to))
 }
 
+// Sr adds a strictly right of filter condition to the request.
 func (b *FilterRequestBuilder) Sr(column string, from, to int) *FilterRequestBuilder {
 	return b.Filter(column, "sr", fmt.Sprintf("(%d,%d)", from, to))
 }
 
+// Nxl adds a not strictly left of filter condition to the request.
 func (b *FilterRequestBuilder) Nxl(column string, from, to int) *FilterRequestBuilder {
 	return b.Filter(column, "nxl", fmt.Sprintf("(%d,%d)", from, to))
 }
 
+// Nxr adds a not strictly right of filter condition to the request.
 func (b *FilterRequestBuilder) Nxr(column string, from, to int) *FilterRequestBuilder {
 	return b.Filter(column, "nxr", fmt.Sprintf("(%d,%d)", from, to))
 }
 
-func (b *FilterRequestBuilder) Adj(column string, from, to int) *FilterRequestBuilder {
-	return b.Filter(column, "adj", fmt.Sprintf("(%d,%d)", from, to))
+// Ad adds an adjacent to filter condition to the request.
+func (b *FilterRequestBuilder) Ad(column string, values []string) *FilterRequestBuilder {
+	sanitized := make([]string, len(values))
+	for i, value := range values {
+		sanitized[i] = SanitizeParam(value)
+	}
+	return b.Filter(column, "ad", fmt.Sprintf("{%s}", strings.Join(sanitized, ",")))
 }
 
+// FilterRequestBuilder represents a builder for SELECT requests.
 type SelectRequestBuilder struct {
 	FilterRequestBuilder
 }
 
-func (b *SelectRequestBuilder) Limit(size int) *SelectRequestBuilder {
-	return b.LimitWithOffset(size, 0)
-}
-
-func (b *SelectRequestBuilder) LimitWithOffset(size int, start int) *SelectRequestBuilder {
-	b.header.Set("Range-Unit", "items")
-	b.header.Set("Range", fmt.Sprintf("%d-%d", start, start+size-1))
+// OrderBy sets the ordering column and direction for the SELECT request.
+func (b *SelectRequestBuilder) OrderBy(column, direction string) *SelectRequestBuilder {
+	b.params.Set("order", column+"."+direction)
 	return b
 }
 
-func (b *SelectRequestBuilder) Single() *SelectRequestBuilder {
-	b.header.Set("Accept", "application/vnd.pgrst.object+json")
+// Range sets the range of rows to be returned for the SELECT request.
+func (b *SelectRequestBuilder) Range(from, to int) *SelectRequestBuilder {
+	b.params.Set("range", fmt.Sprintf("%d-%d", from, to))
+	return b
+}
+
+// SingleRow sets the single row behavior for the SELECT request.
+func (b *SelectRequestBuilder) SingleRow() *SelectRequestBuilder {
+	b.params.Set("single-row", "true")
+	return b
+}
+
+// OnlyPayload sets the only payload behavior for the SELECT request.
+func (b *SelectRequestBuilder) OnlyPayload() *SelectRequestBuilder {
+	b.params.Set("only-payload", "true")
+	return b
+}
+
+// WithoutCount sets the without count behavior for the SELECT request.
+func (b *SelectRequestBuilder) WithoutCount() *SelectRequestBuilder {
+	b.params.Set("without-count", "true")
+	return b
+}
+
+// SingleValue sets the single value behavior for the SELECT request.
+func (b *SelectRequestBuilder) SingleValue() *SelectRequestBuilder {
+	b.params.Set("single-value", "true")
 	return b
 }
